6c7c406f33605d4733240976db8f8e46c319fd40
thomasb151108 <thomasb151108@gmail.com>
2025-11-11 13:18:32 +0000

    Item superclass

--- library.py
+++ library.py
@@ -1,47 +1,62 @@
-# ...existing code...
 def main_run():
     class Library:
         def __init__(self):
             self.catalog = []
-        def add_book(self, book):
-            self.catalog.append(book)
-            print("successfully added", book.title)
+        def add_item(self, item):
+            self.catalog.append(item)
+            print("successfully added", item.title)
             print()
-        def list_books(self):
-            print("--BOOK LIST---------------------------¬")
-            for book in self.catalog:
-                print(book.title)
+        def list_items(self):
+            print("--ITEM LIST---------------------------¬")
+            for item in self.catalog:
+                print(item.title)
             print("---------------------------------------")
             print()
         def display_catalog(self):
             print()
             print("------------------------CATALOG------------------------")
             print()
-            for book in self.catalog:
-                book.display_info()
+            for item in self.catalog:
+                item.display_info()
             print("--------------------------------------------------------")
-    
-    class Book:
-        def __init__(self, title, author, isbn):
+
+    class Item:
+        def __init__(self, title, type):
             self.title = title
+            self.type = type
+
+    class Book(Item):
+        def __init__(self, title, author, isbn, type):
+            super().__init__(title, type)
             self.author = author
             self.isbn = isbn
         def display_info(self):
-            print("**"+ self.title+ "**", "\nauthor:", self.author, "\nisbn:", self.isbn)
+            print("**"+ self.title+ "**", "\nauthor:", self.author, "\nisbn:", self.isbn, "\ntype:", self.type)
             print("\n")
-
+    
+    class DVD(Item):
+        def __init__(self, title, director, runtime, type):
+            super().__init__(title, type)
+            self.director = director
+            self.runtime = runtime
+            self.type = "DVD"
+        def display_info(self):
+            print("**"+ self.title+ "**", "\ndirector:", self.director, "\nruntime:", self.runtime, "\ntype:", self.type)
+            print("\n")
 
     my_library = Library()
 
-    book1 = Book("¦The Hitchhiker's Guide to the Galaxy ¦", "Douglas Adams", "978-0345391803")
-    book2 = Book("¦Pride and Prejudice                  ¦", "Jane Austen", "978-0141439518")
-    book3 = Book("¦Sapiens: A Brief History of Humankind¦", "Yuval Noah Harari", "978-0062316097")
+    book1 = Book("¦The Hitchhiker's Guide to the Galaxy ¦", "Douglas Adams", "978-0345391803", "book")
+    book2 = Book("¦Pride and Prejudice                  ¦", "Jane Austen", "978-0141439518", "book")
+    book3 = Book("¦Sapiens: A Brief History of Humankind¦", "Yuval Noah Harari", "978-0062316097", "book")
+    dvd1 = DVD("¦Thundercats collection               ¦", "Katsuhito Akiyama and Jules Bass", "47 hours and 39 minutes", "DVD")
 
-    my_library.add_book(book1)
-    my_library.add_book(book2)
-    my_library.add_book(book3)
+    my_library.add_item(book1)
+    my_library.add_item(book2)
+    my_library.add_item(book3)
+    my_library.add_item(dvd1)
 
-    my_library.list_books()
+    my_library.list_items()
     my_library.display_catalog()
 
 main_run()
