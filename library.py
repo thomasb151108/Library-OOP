51627ecb9532187c0ebe0aafd9ce6eab99870497
thomasb151108 <thomasb151108@gmail.com>
2025-11-12 10:19:02 +0000

    added game item

--- library.py
+++ library.py
@@ -44,17 +44,28 @@ def main_run():
             print("**"+ self.title+ "**", "\ndirector:", self.director, "\nruntime:", self.runtime, "\ntype:", self.type)
             print("\n")
 
+    class Game(Item):
+        def __init__(self, title, developer, type):
+            super().__init__(title, type)
+            self.developer = developer
+            self.type = "game"
+        def display_info(self):
+            print("**"+ self.title+ "**", "\ndeveloper:", self.developer, "\ntype:", self.type)
+            print("\n")
+
     my_library = Library()
 
     book1 = Book("¦The Hitchhiker's Guide to the Galaxy ¦", "Douglas Adams", "978-0345391803", "book")
     book2 = Book("¦Pride and Prejudice                  ¦", "Jane Austen", "978-0141439518", "book")
     book3 = Book("¦Sapiens: A Brief History of Humankind¦", "Yuval Noah Harari", "978-0062316097", "book")
-    dvd1 = DVD("¦Thundercats collection               ¦", "Katsuhito Akiyama and Jules Bass", "47 hours and 39 minutes", "DVD")
+    dvd1 = DVD(  "¦Thundercats collection               ¦", "Katsuhito Akiyama and Jules Bass", "47 hours and 39 minutes", "DVD")
+    game1 = Game("¦Zelda                                ¦", "Nintendo", "game")
 
     my_library.add_item(book1)
     my_library.add_item(book2)
     my_library.add_item(book3)
     my_library.add_item(dvd1)
+    my_library.add_item(game1)
 
     my_library.list_items()
     my_library.display_catalog()
